7db33e5ebe7df147cc541e4214743e831924e952
c22063583 <whitef6@cardiff.ac.uk>
2023-10-19 14:52:23 +0100

    initial commit

---
+++ src/main/java/uk/ac/cf/spring/demo/PersonalSpringApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/cf/spring/demo/PersonalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.cf.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
